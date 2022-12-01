94607e841c305465deac371e1aeebe795fc41886
talyatown <117781530+talyatown@users.noreply.github.com>
2022-11-30 23:56:10 -0500

    Add files via upload
    project movie list

---
+++ project.py
@@ -0,0 +1,28 @@
+movieDrama = ["The Shawshank Redemption", "Braveheart", "Come and See", "Citizen Kane", "Lawrence of Arabia", "Once Upon a Time in America", "Good Will Hunting", "Requiem for a Dream", "The Boat", "The Prestige", "Grave of the Fireflies", "Modern Times", "Citylight", "Avengers: Endgame", "The Godfather", "Eternal Sunshine of the Spotless Mind" , "Paths of Glory", "The Dark Knight","Amadeus", "High and Low", "The Shining", "The Lion King", "The Lord of the Rings: The Return of the King", "Schindler List", "Cinema Paradiso", "The Godfather pt 2", "Inglourious Basterds","Oldboy", "Witness for the Prosecution", "3 Idiots", "The Lives of Others", "Django Unchained","Top Gun: Maverick", "The Dark Knight Rises", "12 Angry Men", "Pulp Fiction", "Fight Club", "The lord of the rings: The Two Towers", "The lord of the rings:The fellowship of the Ring", "Forrest Gump", "Goodfellas", "One Flew Over the Cuckoo's Nest", "Interstellar", "City of god","American History X", "Saving Private Ryan", "The Green Mile", "Life Is Beautiful", "Se7en", "The Silence of the Lambs", "Harakiri", "Seven Samurai", "It's a Wonderful Life", "Parasite", "Whiplash", "The Intouchables", "Casablanca", "American Beauty", "Departed", "Gladiator", "The Usual Suspects", "Léon: The Professional", "Apocalypse Now", "Capernaum", "Joker", "Your Name.", "Sunset Blvd.", "The Great Dictator", "The Hunt"]
+
+movieCrime = ["The Godfather", "The Dark Knight", "The Godfather pt 2", "Reservoir Dogs", "Once Upon a Time in America", "12 Angry Men", "Pulp Fiction", "Goodfellas" ,"High and Low", "City of God", "The Green Mile", "Se7en", "The Silence of the Lambs", "The Departed", "American History X", "The Usual Suspects", "Léon: The Professional", "Joker", "Witness for the Prosecution", "M"]
+
+movieAction = ["The Dark Knight", "Léon: The Professional", "Indiana Jones and the Raiders of the Lost Ark", "North by Northwest", "Star Wars: Episode VI - Return of the Jedi", "The Lord of the Rings: The Return of the King", "Inception", "Avengers: Infinity War", "Avengers: Endgame", "Gladiator", "Top Gun: Maverick", "The Dark Knight Rises", "Oldboy", "Aliens", "The lord of the rings: The Two Towers", "Spider-Man: Into the Spider-Verse", "Fellowship of the Ring", "The Matrix", "Star Wars: Episode V- The Empire Strikes Back", "Terminator 2: Judgment Day", "Star Wars: Episode IV - A New Hope", "Harakiri", "Seven Samurai", "Princess Mononoke"]
+
+movieAdventure = ["The Lord of the Rings: The Return of the King", "Inception", "North by Northwest", "Lawrence of Arabia", "2001: A Space Odyssey", "Star Wars: Episode VI - Return of the Jedi", "Toy Story", "Inglourious Basterds", "Coco", "Toy Story 3", "Indiana Jones and the Raiders of the Lost Ark", "Avengers: Infinity War", "Aliens", "Avengers: Endgame", "Back to the Future", "The lord of the rings: The Two Towers", "The lord of the rings:The fellowship of the Ring", "The good, the bad, and the ugly", "Star Wars: Episode V- The Empire Strikes Back", "Interstellar", "Spirited Away", "Star Wars: Episode IV - A New Hope", "Gladiator", "The Lion King", "Spider-Man: Into the Spider-Verse", "WALL·E", "Princess Mononoke"]
+
+movieScifi = ["Inception", "2001: A Space Odyssey", "The good, the bad, and the ugly", "The Matrix", "Interstellar", "Terminator 2: Judgment Day", "The Prestige", "Back to the Future", "Alien", "Avengers: Infinity War", "Aliens", "Eternal Sunshine of the Spotless Mind"]
+
+movieBiography = ["Schindler List", "Goodfellas", "The Intouchables", "Braveheart", "Amadeus", "Lawrence of Arabia"]
+
+movieRomance = ["Forrest Gump", "Life Is Beautiful", "Cinema Paradiso", "Casablanca", "Modern Times", "City lights", "Eternal Sunshine of the Spotless Mind", "Good Will Hunting", "Vertigo", "Singin' in the Rain"]
+
+movieFantasy = ["Star Wars: Episode V- The Empire Strikes Back", "The Green Mile", "Star Wars: Episode IV - A New Hope", "Harakiri", "It's a Wonderful Life", "Your Name.", "Star Wars: Episode VI - Return of the Jedi"]
+
+movieHorror = ["Alien", "Psych", "The Shining"]
+
+movieAnimation = ["Spirited Away", "The Lion King", "Grave of the Fireflies", "Coco", "Your Name.", "Toy Story 3", "Toy Story", "Spider-Man: Into the Spider-Verse", "WALL·E", "Princess Mononoke"]
+
+movieWar = ["Saving Private Ryan", "Grave of the Fireflies", "Apocalypse Now", "Casablanca", "Inglourious Basterds", "Come and See", "The Boat", "The Great Dictator", "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb", "Paths of Glory]
+            
+movieComedy = ["Life Is Beautiful", "Coco", "The Intouchable", "Back to the Future", "Modern Times", "Toy Story", "City lights", "3 Idiots", "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb", "The Great Dictator", "Toy Story 3", "Singin' in the Rain"]
+
+movieMystery = ["Se7en", "The Prestige", "The Usual Suspects", "Apocalypse Now", "Psycho", "Rear Window", "The Lives of Others", "Oldboy", "Memento", "High and Low", "M", "Witness for the Prosecution", "North by Northwest", "Vertigo", "Citizen Kane", "Se7en"]
+
+movieThriller = ["The Silence of the Lambs", "Parasite", "Psycho", "Rear Window", "Joker", "The Lives of Others", "Memento", "M", "Come and See", "Reservoir Dogs", "Vertigo"]
+
